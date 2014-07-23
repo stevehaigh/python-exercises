fc3d6ee81c5c05a9d7c037958c0fc256a1362e4d
Steve Haigh <steve_a_haigh@hotmail.com>
2014-07-23 22:48:52 +0100

    tidy up error checks

--- dna.py
+++ dna.py
@@ -1,25 +1,25 @@
+# Count the number of each base A,C,G and T in a file.
+#
 import sys
 
-filename = sys.argv[1]
+if len(sys.argv) < 2 or not sys.argv[1]:
+    print "Please specify file containing a DNA sequence"
+    sys.exit()
 
-if not filename:
-	print "Please specify file containing a DNA sequence"
-	sys.exit
+seq = open(sys.argv[1], "r").read()
 
-seq = open(filename, "r").read()
-
-# Use a dictionary to store counts. Could use seperate variables but this
+# Use a dictionary to store counts. Could use separate variables but this
 # saves a set of 'if' tests as we can reference the counts directly.
-baseCounts = { 'A' : 0, 'C' : 0, 'G' : 0, 'T' : 0}
+baseCounts = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
 for base in seq:
-	if base in baseCounts:
-		baseCounts[base] += 1
-	else:
-		sys.exit("invalid base letter found in file.")
+    if base in baseCounts:
+        baseCounts[base] += 1
+    else:
+        sys.exit("invalid base letter found in file.")
 
 # Print the items by key, iterating is not a good idea as the order 
-# is not determinstic.
+# is not deterministic.
 print baseCounts['A'],
 print baseCounts['C'],
 print baseCounts['G'],
