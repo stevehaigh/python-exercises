faf52ca309d69057ba72ee3a60d0832a8990bd73
Steve Haigh <steve.haigh@gmail.com>
2014-08-11 18:08:41 +0100

    tidying

--- revc.py
+++ revc.py
@@ -1,21 +1,18 @@
-# Convert a DNA sequence to corresponding reverse compliment sequence.
-#
 import sys
 
 if len(sys.argv) < 2 or not sys.argv[1]:
     print "Please specify file containing a DNA sequence"
     sys.exit()
 
-DNAseq = open(sys.argv[1], "r").read()
+DNA_seq = open(sys.argv[1], "r").read()
 
 # reverse the sequence using the extended slice method. make it lower case too to aid in the base switching next.
-rev = DNAseq[::-1].lower()
+rev = DNA_seq[::-1].lower()
 
-# now convert a -> T, t -> A, c -> G, g -> C
-
+# convert a -> T, t -> A, c -> G, g -> C
 revc = rev.replace('a', 'T').replace('t', 'A').replace('c', 'G').replace('g', 'C')
 
-with open("revc.txt", "w") as revfile:
-    revfile.write(revc)
+with open("revc.out.txt", "w") as rev_file:
+    rev_file.write(revc)
 
 print "done"
